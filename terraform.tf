048c011e293f67e4101040a3010dbcd1f6e400d9
Chris Rose <offline@offby1.net>
2018-07-03 07:12:09 -0700

    Add  Subnet

--- terraform.tf
+++ terraform.tf
@@ -70,3 +70,43 @@ resource "aws_security_group" "vpc-6a628612-default" {
 
 }
 
+resource "aws_subnet" "subnet-8717f6e2-subnet-8717f6e2" {
+    vpc_id                  = "vpc-b0bca7d2"
+    cidr_block              = "10.0.0.0/24"
+    availability_zone       = "us-west-2a"
+    map_public_ip_on_launch = false
+
+    tags {
+    }
+}
+
+resource "aws_subnet" "subnet-11dc455a-subnet-11dc455a" {
+    vpc_id                  = "vpc-6a628612"
+    cidr_block              = "172.30.1.0/24"
+    availability_zone       = "us-west-2b"
+    map_public_ip_on_launch = true
+
+    tags {
+    }
+}
+
+resource "aws_subnet" "subnet-57632b0d-subnet-57632b0d" {
+    vpc_id                  = "vpc-6a628612"
+    cidr_block              = "172.30.2.0/24"
+    availability_zone       = "us-west-2c"
+    map_public_ip_on_launch = true
+
+    tags {
+    }
+}
+
+resource "aws_subnet" "subnet-fbb6ca82-subnet-fbb6ca82" {
+    vpc_id                  = "vpc-6a628612"
+    cidr_block              = "172.30.0.0/24"
+    availability_zone       = "us-west-2a"
+    map_public_ip_on_launch = true
+
+    tags {
+    }
+}
+
