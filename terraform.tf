ec6d8b2611c7bf7b39f4df08fe12ab0124a44f1b
Chris Rose <offline@offby1.net>
2018-07-03 07:03:54 -0700

    Terraform base state

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-west-2"
+}
+
+variable "domain_name" {
+  default = "chicagoworldcon.org"
+}
+
+resource "aws_vpc" "vpc-6a628612" {
+  cidr_block           = "172.30.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  instance_tenancy     = "default"
+
+  tags {
+  }
+}
