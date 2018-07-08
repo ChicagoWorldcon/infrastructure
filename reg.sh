154e9036c3f5095f856b0bf9ce778741040d84b7
Chris Rose <offline@offby1.net>
2018-07-07 22:24:15 -0700

    Convenient accessor for the remote host over a bastion

---
+++ reg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bastion_ip=$(terraform output bastion_public_ip)
+registration_ip=$(terraform output reg_private_ip)
+#exec ssh -oProxyCommand="ssh ec2-user@${bastion_ip} -W %h:%p" ec2-user@${registration_ip}
+exec ssh -A -J ec2-user@${bastion_ip} ec2-user@${registration_ip}
