630188a404cfdfb1e396b8bf99303fc2588f5be3
Chris Rose <offline@offby1.net>
2018-07-21 07:37:40 -0700

    Stop using ACM Vend the DNS zone for use in reg/letsencrypt

--- global/dns.tf
+++ global/dns.tf
@@ -7,13 +7,6 @@ output "name_servers" {
   value = "${aws_route53_zone.main.name_servers}"
 }
 
-resource "aws_acm_certificate" "certificate" {
-  // We want a wildcard cert so we can host subdomains later.
-  domain_name       = "*.${var.domain_name}"
-  validation_method = "EMAIL"
-
-  // We also want the cert to be valid for the root domain even though we'll be
-  // redirecting to the www. domain immediately.
-  subject_alternative_names = ["${var.domain_name}"]
+output "dns_zone_id" {
+  value = "${aws_route53_zone.main.zone_id}"
 }
-
