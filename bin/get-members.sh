429638d9f54fbe580e355161ebc9f83e0088ffa5
Chris Rose <offline@offby1.net>
2020-06-15 07:34:35 -0700

    More robust member getter

--- bin/get-members.sh
+++ bin/get-members.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+set -ue
 stage="${1:?stage argument first}"; shift
-HERE=$(unset CDPATH; cd $(dirname $0); pwd)
-exec ssh ec2-user@$(cd $HERE && terraform output -json reg_public_dns | jq -r ".${stage}") psql.reg -c "\"COPY (select legal_name, public_first_name, public_last_name, email, contact_prefs, membership, member_number, key from members.people left join members.keys using (email) WHERE membership <> 'NonMember' and email NOT LIKE '%replace.me') TO STDOUT WITH CSV HEADER;\""
+script_file=$(mktemp)
+dest_script_file=$(ssh ec2-user@reg-${stage} mktemp)
+cat<<EOF>$script_file
+COPY (
+  select legal_name,
+         CASE
+         WHEN public_first_name IS NOT NULL THEN 't'
+         ELSE NULL
+         END as make_public,
+         public_first_name,
+         public_last_name,
+         badge_text,
+         email,
+         contact_prefs,
+         membership,
+         member_number,
+         key
+    FROM members.people
+           LEFT JOIN members.keys USING (email)
+   WHERE membership <> 'NonMember'
+     AND email NOT LIKE '%replace.me'
+) TO STDOUT WITH CSV HEADER;
+EOF
+
+scp $script_file reg-${stage}:$dest_script_file
+ssh reg-${stage} psql.reg -f $dest_script_file 
