d5d904e12d92123ddaa35e5d86559f189a205d80
Chris Rose <offline@offby1.net>
2020-07-07 00:51:40 -0700

    Add the registration DNS name

--- hosting/dns/main.tf
+++ hosting/dns/main.tf
@@ -12,3 +12,11 @@ resource "aws_route53_record" "dashboard" {
   ttl     = 300
   records = [var.infra_host]
 }
+
+resource "aws_route53_record" "registration" {
+  zone_id = var.dns_zone_id
+  type    = "CNAME"
+  name    = "registration${var.suffix}"
+  ttl     = 300
+  records = [var.infra_host]
+}
