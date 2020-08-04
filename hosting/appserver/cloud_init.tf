30b8710c916446dd5901b2da51239c2e909701ef
Chris Rose <offline@offby1.net>
2020-08-04 07:55:51 -0700

    Configure docker to log to journald
    Part of #2

--- hosting/appserver/cloud_init.tf
+++ hosting/appserver/cloud_init.tf
@@ -5,7 +5,6 @@ data "template_file" "script" {
     # service initializationa and config
     instance_launch_script    = base64encode("${data.template_file.bootstrap.rendered}")
     install_codedeploy_script = base64encode("${data.template_file.install_codedeploy.rendered}")
-    docker_daemon_json        = base64encode("${data.template_file.docker_daemon_json.rendered}")
   }
 }
 
@@ -18,14 +17,6 @@ data "template_file" "install_codedeploy" {
 
   vars = {
     codedeploy_agent_s3_bucket = "aws-codedeploy-us-west-2.s3.amazonaws.com"
-  }
-}
-
-data "template_file" "docker_daemon_json" {
-  template = file("${path.module}/scripts/docker-daemon-${var.docker_log_driver}.json")
-
-  vars = {
-    log_group = aws_cloudwatch_log_group.registration_group.name
   }
 }
 
