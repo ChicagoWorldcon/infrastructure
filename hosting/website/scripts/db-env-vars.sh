f77b4f3cf58df26cbdd6e2721c2f9fec4d2d223e
Chris Rose <offline@offby1.net>
2020-08-05 23:57:28 -0700

    Remove obsolete website dir

--- hosting/website/scripts/db-env-vars.sh
+++
@@ -1,4 +0,0 @@
-export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id ${db_site_secret} | jq -r .SecretString)
-export KANSA_PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${project}/db/${db_name}/kansa/${stage} | jq -r .SecretString)
-export HUGO_PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${project}/db/${db_name}/hugo/${stage} | jq -r .SecretString)
-export RAAMI_PG_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${project}/db/${db_name}/raami/${stage} | jq -r .SecretString)
--- hosting/website/tmp-install-codedeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum install -y ruby wget
-cd $HOME
-wget aws-codedeploy-us-west-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
