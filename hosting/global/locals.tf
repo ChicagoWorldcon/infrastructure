382b141275787dadad40c3691ce93b6abb83c35f
Chris Rose <offline@offby1.net>
2021-01-25 08:12:48 -0800

    Remove obsolete global state in hosting

--- hosting/global/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags = {
-    Project = "${var.project}"
-  }
-
-  dev_api_host  = "${var.dev_api_host_prefix}.${var.domain_name}:443"
-  prod_api_host = "${var.prod_api_host_prefix}.${var.domain_name}:443"
-}
--- hosting/global/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dev_deployment_group" {
-  value = aws_codedeploy_deployment_group.dev.deployment_group_name
-}
-
-output "prod_deployment_group" {
-  value = aws_codedeploy_deployment_group.prod.deployment_group_name
-}
