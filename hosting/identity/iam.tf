6318eba808ded431d4d1e695fba4bc7f15e8aac8
Chris Rose <offline@offby1.net>
2021-02-01 22:48:27 -0800

    More generic names in the hosting identity

--- hosting/identity/iam.tf
+++ hosting/identity/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "registration" {
+resource "aws_iam_role" "instance" {
   name               = "${var.project}-${var.application}-${var.stage}"
   assume_role_policy = <<EOF
 {
@@ -17,9 +17,9 @@ resource "aws_iam_role" "registration" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "registration" {
+resource "aws_iam_instance_profile" "instance" {
   name = "${var.project}-${var.application}-instance-profile-${var.stage}"
-  role = aws_iam_role.registration.name
+  role = aws_iam_role.instance.name
 }
 
 data "template_file" "hosting-role-policy" {
@@ -33,15 +33,15 @@ data "template_file" "hosting-role-policy" {
   }
 }
 
-resource "aws_iam_role_policy" "registration" {
+resource "aws_iam_role_policy" "instance" {
   name   = "${var.project}-${var.application}-policy-${var.stage}"
-  role   = aws_iam_role.registration.name
+  role   = aws_iam_role.instance.name
   policy = data.template_file.hosting-role-policy.rendered
 }
 
 resource "aws_iam_role_policy" "deployment" {
   name   = "${var.project}-api-deployment-policy"
-  role   = aws_iam_role.registration.name
+  role   = aws_iam_role.instance.name
   policy = <<EOF
 {
     "Version": "2012-10-17",
--- hosting/identity/outputs.tf
+++ hosting/identity/outputs.tf
@@ -1,7 +1,7 @@
 output "iam_instance_profile_id" {
-  value = aws_iam_instance_profile.registration.id
+  value = aws_iam_instance_profile.instance.id
 }
 
 output "iam_role_name" {
-  value = aws_iam_role.registration.name
+  value = aws_iam_role.instance.name
 }
