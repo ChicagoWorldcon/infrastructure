361f2e23ff753f29ee874836471eda524305de84
Chris Rose <offline@offby1.net>
2024-01-13 21:22:43 -0800

    Clean up leftover hosting credentials

--- hosting/appserver/dns.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "a_record_org" {
-  zone_id = var.dns_zone_id
-  name    = var.www_domain_name
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.web.public_ip]
-}
-
--- main.tf
+++ main.tf
@@ -2,24 +2,6 @@
 # site hosts are in registration.tf
 # DB is in db.tf
 
-module "hosting" {
-  source      = "./hosting/"
-  project     = var.project
-  region      = var.region
-  dns_zone_id = data.aws_route53_zone.chicon.zone_id
-  domain_name = var.domain_name
-
-  vpc_id               = module.vpc.vpc_id
-  vpc_public_subnet_id = module.vpc.public_subnets[0]
-
-  ssh_key_id = var.ssh_key_id
-
-  security_group_id = module.vpc.default_security_group_id
-
-  codedeploy_bucket = module.global.artifact_bucket
-
-}
-
 module "hugo-packet" {
   source  = "./hugo-bucket/"
   project = var.project
